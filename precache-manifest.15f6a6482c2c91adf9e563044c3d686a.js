9e323572206d0534b0b3817574c88e8344e20af6
Anton Azhhirevych <azhhirevych943@gmail.com>
2019-09-29 22:04:20 +0300

    Updates

---
+++ precache-manifest.15f6a6482c2c91adf9e563044c3d686a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd9bd01e5783872fd717f5b648d76e4e",
+    "url": "/agilenix-usersList/index.html"
+  },
+  {
+    "revision": "c54a9d3939145a67c187",
+    "url": "/agilenix-usersList/static/css/2.bc3aac6f.chunk.css"
+  },
+  {
+    "revision": "c54a9d3939145a67c187",
+    "url": "/agilenix-usersList/static/js/2.68ec1cbe.chunk.js"
+  },
+  {
+    "revision": "0c330f4e56f1adb2a187",
+    "url": "/agilenix-usersList/static/js/main.45dac96b.chunk.js"
+  },
+  {
+    "revision": "a037fd6e7a1f7931366b",
+    "url": "/agilenix-usersList/static/js/runtime-main.517ec398.js"
+  }
+]);
